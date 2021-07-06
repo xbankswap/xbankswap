a8571fcf657f1f3b2dd326a7f0838823eb66dea6
free2swap <ddcommune@gmail.com>
2021-07-06 14:57:05 +0800

    first commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,10 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  't2crtokens.eth', // kleros
+  'tokens.1inch.eth', // 1inch
+  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  'https://xbank.cf/wp-content/uploads/2021/07/xbankswap.json'
+]
